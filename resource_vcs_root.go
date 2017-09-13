78c9d5d2e6e44ff64cce7d33858684c466f19178
Techcadia <info@techcadia.com>
2017-09-13 16:06:11 -0700

    [Add] Support for Replace

--- resource_vcs_root.go
+++ resource_vcs_root.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform/helper/schema"
 
-	"errors"
+	// "errors"
 	"github.com/Cardfree/teamcity-sdk-go/teamcity"
 	"github.com/Cardfree/teamcity-sdk-go/types"
 	"log"
@@ -57,65 +57,25 @@ func resourceVcsRoot() *schema.Resource {
 func resourceVcsRootCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*teamcity.Client)
 
-	// d.Partial(true)
-	project := d.Get("project").(string)
-	name := d.Get("name").(string)
-	vcs_provider := d.Get("vcs_provider").(string)
+	vcsProject := d.Get("project").(string)
+	vcsName := d.Get("name").(string)
+	vcsProvider := d.Get("vcs_provider").(string)
+	vcsProperties := d.Get("properties").(map[string]string)
 
 	vcs := types.VcsRoot{
-		ProjectID: types.ProjectId(project),
-		Name:      name,
-		VcsName:   vcs_provider,
+		ProjectID:  types.ProjectId(vcsProject),
+		Name:       vcsName,
+		VcsName:    vcsProvider,
+		Properties: vcsProperties,
 	}
 	err := client.CreateVcsRoot(&vcs)
 	if err != nil {
 		return err
 	}
+
 	id := vcs.ID
 	d.SetId(id)
 	d.Set("vcs_provider", vcs.VcsName)
-	// d.SetPartial("project")
-	// d.SetPartial("name")
-	// d.SetPartial("vcs_provider")
-
-	// if parent == "" {
-	// 	parent = "_Root"
-	// }
-	// var parent_parameters types.Parameters
-	// if parent_project, err := client.GetProject(parent); err != nil {
-	// 	return err
-	// } else {
-	// 	parent_parameters = parent_project.Parameters
-	// }
-
-	// parameters := definitionToParameters(*d.Get("parameter").(*schema.Set))
-	// for name, _ := range parameters {
-	// 	if parent_parameter, ok := parent_parameters[name]; ok && parent_parameter.Spec != nil {
-	// 		return fmt.Errorf("Can't redefine parent parameter %s", name)
-	// 	}
-	// }
-	// for name, v := range d.Get("parameter_values").(map[string]interface{}) {
-	// 	value := v.(string)
-	// 	parameter, ok := parameters[name]
-	// 	if !ok {
-	// 		if parameter, ok = parent_parameters[name]; !ok {
-	// 			parameter = types.Parameter{
-	// 				Value: value,
-	// 			}
-	// 		}
-	// 	}
-	// 	parameter.Value = value
-	// 	parameters[name] = parameter
-	// 	log.Printf("Parameter value %s => %s", name, parameter.Value)
-	// }
-	// log.Printf("Replace Parameters value %q", parameters)
-	// if err := client.ReplaceAllProjectParameters(id, &parameters); err != nil {
-	// 	return err
-	// }
-	// d.SetPartial("parameter_values")
-	// d.SetPartial("parameter")
-
-	// d.Partial(false)
 	return nil
 }
 
@@ -131,119 +91,30 @@ func resourceVcsRootRead(d *schema.ResourceData, meta interface{}) error {
 		return nil
 	}
 
-	// parent := vcs.ParentProjectID
-	// if parent == "_Root" {
-	// 	parent = ""
-	// }
-
 	d.Set("project", vcs.ProjectID)
 	d.Set("name", vcs.Name)
 	d.Set("vcs_provider", vcs.VcsName)
-
-	// var parent_parameters types.Parameters
-	// if parent_project, err := client.GetProject(string(project.ParentProjectID)); err != nil {
-	// 	return err
-	// } else {
-	// 	parent_parameters = parent_project.Parameters
-	// }
-	// parameters := project.Parameters
-	// values := make(map[string]interface{})
-	// current := d.Get("parameter_values").(map[string]interface{})
-	// for name, parameter := range project.Parameters {
-	// 	if parent_parameter, ok := parent_parameters[name]; ok {
-	// 		if parent_parameter.Value != parameter.Value {
-	// 			values[name] = parameter.Value
-	// 		}
-	// 		if parent_parameter.Spec != nil || parameter.Spec == nil {
-	// 			delete(parameters, name)
-	// 		}
-	// 	} else {
-	// 		if parameter.Spec == nil {
-	// 			delete(parameters, name)
-	// 		}
-	// 		pwt := types.PasswordType{}
-	// 		if parameter.Value != "" {
-	// 			values[name] = parameter.Value
-	// 		} else if parameter.Spec != nil && parameter.Spec.Type == pwt {
-	// 			if value, ok := current[name]; ok && value != "" {
-	// 				values[name] = value
-	// 			}
-	// 		}
-	// 	}
-	// }
-	// d.Set("parameter", parametersToDefinition(parameters))
-	// d.Set("parameter_values", values)
 
 	return nil
 }
 
 func resourceVcsRootUpdate(d *schema.ResourceData, meta interface{}) error {
-	return errors.New("Update is not supported")
-	// client := meta.(*teamcity.Client)
+	// return errors.New("Update is not supported")
+	client := meta.(*teamcity.Client)
 
-	// id := d.Id()
-	// d.Partial(true)
+	id := d.Id()
+	d.Partial(true)
 
-	// if d.HasChange("description") {
-	// 	if err := client.SetProjectDescription(d.Id(), d.Get("description").(string)); err != nil {
-	// 		return err
-	// 	}
-	// 	d.SetPartial("description")
-	// }
+	vcsProperties := types.Properties(d.Get("properties").(map[string]string))
 
-	// if d.HasChange("parameter") || d.HasChange("parameter_values") {
-	// 	parent := d.Get("parent").(string)
-	// 	if parent == "" {
-	// 		parent = "_Root"
-	// 	}
-	// 	var parent_parameters types.Parameters
-	// 	if parent_project, err := client.GetProject(parent); err != nil {
-	// 		return err
-	// 	} else {
-	// 		parent_parameters = parent_project.Parameters
-	// 	}
+	if d.HasChange("properties") {
+		if err := client.ReplaceAllVcsRootProperties(id, &vcsProperties); err != nil {
+			return err
+		}
+		d.SetPartial("properties")
+	}
 
-	// 	o, n := d.GetChange("parameter")
-	// 	parameters := definitionToParameters(*n.(*schema.Set))
-	// 	old := definitionToParameters(*o.(*schema.Set))
-	// 	replace_parameters := make(types.Parameters)
-	// 	delete_parameters := old
-	// 	for name, parameter := range parameters {
-	// 		if parent_parameter, ok := parent_parameters[name]; ok && parent_parameter.Spec != nil {
-	// 			return fmt.Errorf("Can't redefine parent parameter %s", name)
-	// 		}
-	// 		if !reflect.DeepEqual(parameter, old[name]) {
-	// 			replace_parameters[name] = parameter
-	// 		}
-	// 		delete(delete_parameters, name)
-	// 	}
-	// 	for name, v := range d.Get("parameter_values").(map[string]interface{}) {
-	// 		value := v.(string)
-	// 		parameter, ok := parameters[name]
-	// 		if !ok {
-	// 			if parameter, ok = parent_parameters[name]; !ok {
-	// 				parameter = types.Parameter{
-	// 					Value: value,
-	// 				}
-	// 			}
-	// 		}
-	// 		parameter.Value = value
-	// 		replace_parameters[name] = parameter
-	// 	}
-	// 	for name, _ := range delete_parameters {
-	// 		if err := client.DeleteProjectParameter(id, name); err != nil {
-	// 			return err
-	// 		}
-	// 	}
-	// 	for name, parameter := range replace_parameters {
-	// 		if err := client.ReplaceProjectParameter(id, name, &parameter); err != nil {
-	// 			return err
-	// 		}
-	// 	}
-	// 	d.SetPartial("parameter_values")
-	// 	d.SetPartial("parameter")
-	// }
-
+	d.Partial(false)
 	return nil
 }
 
